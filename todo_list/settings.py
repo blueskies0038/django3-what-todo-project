5fec415d44f71b4897d685ff46bdee2fbb97e469
Jenny <jennybao1004@gmail.com>
2020-07-24 23:35:12 +0000

    Removed old stuff

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(d@vk1@c7#myaoi&3_gnt-_gyv#4p9q_3lr(i%4#cx$e&!-_+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blueskies0038.pythonanywhere.com']
 
 
 # Application definition
@@ -119,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/login'
